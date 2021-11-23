31c9dbe616b6d6fc05f410b5ec5a15e087b3b2b3
sarunas <sarunas.guob@gmail.com>
2021-11-23 21:11:41 +0200

    refactor: Duck to game detail
    #

--- client/src/state/reducers/gameDetails/index.ts
+++ client/src/state/reducers/gameDetails/index.ts
@@ -1,6 +1,6 @@
 import { createClearData, createFetchData } from "../_ducks/fetchData";
 
-export const REDUCER_NAME = "CURRENT_GAME";
+export const REDUCER_NAME = "GAME_DETAIL";
 
 export const clearCurrentGame = createClearData(REDUCER_NAME);
 export const fetchCurrentGame = createFetchData(REDUCER_NAME, ({ id }) => {
--- client/src/state/reducers/index.ts
+++ client/src/state/reducers/index.ts
@@ -1,11 +1,11 @@
 import { createNamedWrapperReducer } from "./helpers";
 import fetchDataReducer from "./_ducks/fetchData";
 
-import { REDUCER_NAME as CURRENT_GAME_NAME } from "./currentGame";
+import { REDUCER_NAME as GAME_DETAIL_NAME } from "./gameDetails";
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
-  currentGame: createNamedWrapperReducer(fetchDataReducer, CURRENT_GAME_NAME),
+  gameDetail: createNamedWrapperReducer(fetchDataReducer, GAME_DETAIL_NAME),
 });
 
 export default reducers;
